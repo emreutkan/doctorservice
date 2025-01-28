011f7edc386f44fe92a29c7a9df2abe21e4a6bb0
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-01-28 10:51:04 +0300

    role

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,12 +17,19 @@ const loginHandler = async (req, res) => {
 
         const token = generateToken({
             doctorId: doctor._id.toString(),
-            username: doctor.username
-        });        res.status(200).json({ message: 'Login successful', token });
+            username: doctor.username,
+        });
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            role: doctor.role,
+        });
     } catch (error) {
         res.status(500).json({ message: 'Error logging in', error });
     }
 };
+
 
 const registerHandler = async (req, res) => {
     const { username, email, password } = req.body;
@@ -30,10 +37,15 @@ const registerHandler = async (req, res) => {
     try {
         const newDoctor = new Doctor({ username, email, password });
         await newDoctor.save();
-        res.status(201).json({ message: 'Doctor registered successfully' });
+
+        res.status(201).json({
+            message: 'Doctor registered successfully',
+            role: newDoctor.role,
+        });
     } catch (error) {
         res.status(500).json({ message: 'Error registering doctor', error });
     }
 };
+
 
 module.exports = { loginHandler, registerHandler };
--- models/doctorModel.js
+++ models/doctorModel.js
@@ -5,6 +5,7 @@ const doctorSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    role: { type: String, default: 'doctor' }, // Default role
 });
 
 doctorSchema.pre('save', async function (next) {
